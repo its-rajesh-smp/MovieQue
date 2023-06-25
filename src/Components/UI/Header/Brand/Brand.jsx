6958a38c2f4c3d0d9464d2d249202c776a010d72
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-06-25 23:52:39 +0530

    Added Logo

--- src/Components/UI/Header/Brand/Brand.jsx
+++ src/Components/UI/Header/Brand/Brand.jsx
@@ -4,10 +4,8 @@ import "./Brand.css";
 function Brand(props) {
   return (
     <div className=" Brand-div ">
-      <img
-        src="https://img.hotstar.com/image/upload/v1656431456/web-images/logo-d-plus.svg"
-        alt="imageBrand"
-      />
+      <h1>M</h1>
+      <p>Movie Q</p>
     </div>
   );
 }
